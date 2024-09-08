7ee436327f91726e8171206314b4782922d2366b
MaciejkaG <me@mcjk.cc>
2024-09-08 20:11:41 +0200

    Improved local file list and added interval syncrhonisation

--- src/static/assets/js/renderer.js
+++ src/static/assets/js/renderer.js
@@ -7,17 +7,28 @@ document.getElementById('maximise').addEventListener('click', () => window.api.i
 document.getElementById('close').addEventListener('click', () => window.api.invoke('close'));
 
 window.addEventListener('load', async () => {
-    const noteList = await window.api.invoke('note-list');
-
-    noteList.forEach(note => {
-        const escapedPath = escapeHTML(note.path).replace(/\\/g, '\\\\');
-        document.getElementById('localFilesList').innerHTML += `<span onclick="openEditorFile(\`${escapedPath}\`)">${escapeHTML(note.title)}</span>`;
-    });
+    await syncLocalFileList();
 });
 
 async function getFile(filePath) {
     return await window.api.invoke('note-content', filePath);
 }
+
+async function syncLocalFileList() {
+    const noteList = await window.api.invoke('note-list');
+
+    document.getElementById('localFilesList').innerHTML = '';
+    noteList.forEach(note => {
+        const escapedPath = escapeHTML(note.path).replace(/\\/g, '\\\\');
+        document.getElementById('localFilesList').innerHTML += `<span onclick="openEditorFile(\`${escapedPath}\`)">${escapeHTML(note.title)}</span>`;
+    });
+
+    if (!noteList.length) {
+        document.getElementById('localFilesList').innerHTML = `<span style="cursor:initial;">No local files found</span>`;
+    }
+}
+
+setInterval(syncLocalFileList, 3000);
 
 function escapeHTML(unsafe) {
     return unsafe
